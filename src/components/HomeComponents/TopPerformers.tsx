99ebfc85f6cff1861144756971a81d5d872e2a68
mjaumi <mjaumi2864@gmail.com>
2023-10-03 03:43:19 +0600

    top performers responsiveness done

--- src/components/HomeComponents/TopPerformers.tsx
+++ src/components/HomeComponents/TopPerformers.tsx
@@ -14,14 +14,14 @@ const TopPerformers = () => {
       <div className='w-[90%] lg:w-4/5 max-w-[1500px] mx-auto space-y-10'>
         <SectionTitle title='top performers' />
 
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-12'>
+        <div className='grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-5 xl:gap-12'>
           <TopPerformerItem
             title='Batsman'
             playerName='Babar Azam'
             imgSrc='https://resources.pulse.icc-cricket.com/players/210/2759.png'
             stats={topBatsmanData}
-            imgStyle='-mr-8 w-[210px] md:w-fit'
-            additionalClassName='md:mt-40'
+            imgStyle='-mr-8 w-[190px]'
+            additionalClassName='2xl:mt-40'
           />
           <TopPerformerItem
             title='Team'
@@ -30,16 +30,16 @@ const TopPerformers = () => {
             stats={topTeamData}
             borderColor='border-KKPL-light-blue'
             textColor='text-KKPL-light-blue'
-            imgStyle='w-[100px] h-[100px] md:w-[150px] md:h-[150px] mr-5 mb-5'
-            additionalClassName='md:mb-40'
+            imgStyle='w-[100px] h-[100px] md:w-[120px] md:h-[120px] xl:w-[150px] xl:h-[150px] mr-5 mb-5'
+            additionalClassName='2xl:mb-40'
           />
           <TopPerformerItem
             title='Bowler'
             playerName='Mujeeb Ur Rahman'
             imgSrc='https://resources.pulse.icc-cricket.com/players/210/4572.png'
             stats={topBowlerData}
-            imgStyle='-mr-8 w-[210px] md:w-fit'
-            additionalClassName='md:mt-40'
+            imgStyle='-mr-8 w-[190px]'
+            additionalClassName='2xl:mt-40 col-span-1 md:col-span-2 2xl:col-span-1'
           />
         </div>
       </div>
