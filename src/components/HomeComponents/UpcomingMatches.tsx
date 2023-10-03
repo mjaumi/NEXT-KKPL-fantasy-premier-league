02c78c6dacd9be46ff14bb13123a32951a88484a
mjaumi <mjaumi2864@gmail.com>
2023-10-03 14:13:36 +0600

    fixed upcoming matches a bit

--- src/components/HomeComponents/UpcomingMatches.tsx
+++ src/components/HomeComponents/UpcomingMatches.tsx
@@ -8,22 +8,24 @@ import { match2CardData } from '@/constants/matchCardData';
 const UpcomingMatches = () => {
   // rendering upcoming matches component here
   return (
-    <section className='relative w-[90%] lg:w-4/5 max-w-[1500px] mx-auto py-20 mb-20 space-y-10'>
-      <SectionTitle title='Upcoming Matches' />
+    <section className='py-20 space-y-10'>
+      <div className='relative w-[90%] lg:w-4/5 max-w-[1500px] mx-auto space-y-10'>
+        <SectionTitle title='Upcoming Matches' />
 
-      <div className='space-y-8'>
-        <MatchCard matchData={match2CardData} />
-        <MatchCard matchData={match2CardData} />
-        <MatchCard matchData={match2CardData} />
-        <MatchCard matchData={match2CardData} />
-        <MatchCard matchData={match2CardData} />
-      </div>
+        <div className='space-y-8'>
+          <MatchCard matchData={match2CardData} />
+          <MatchCard matchData={match2CardData} />
+          <MatchCard matchData={match2CardData} />
+          <MatchCard matchData={match2CardData} />
+          <MatchCard matchData={match2CardData} />
+        </div>
 
-      <div className='absolute h-1/3 w-full bottom-0 bg-gradient-to-t from-KKPL-dark-blue z-20 to-transparent flex justify-center items-end'>
-        <KKPLButton>
-          view all upcoming matches
-          <BsArrowRight className='w-7 h-7 ml-2' />
-        </KKPLButton>
+        <div className='absolute h-1/3 w-full bottom-0 bg-gradient-to-t from-KKPL-dark-blue z-20 to-transparent flex justify-center items-end'>
+          <KKPLButton>
+            view all upcoming matches
+            <BsArrowRight className='w-7 h-7 ml-2' />
+          </KKPLButton>
+        </div>
       </div>
     </section>
   );
