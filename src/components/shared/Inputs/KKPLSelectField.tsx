6a05137fb91aaa799b15d667a38dc765cb2c694f
mjaumi <mjaumi2864@gmail.com>
2023-10-07 20:20:09 +0600

    created custom select field component

---
+++ src/components/shared/Inputs/KKPLSelectField.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const KKPLSelectField = () => {
+  // rendering the custom select field here
+  return (
+    <select name='' id=''>
+      select a team
+    </select>
+  );
+};
+
+export default KKPLSelectField;
