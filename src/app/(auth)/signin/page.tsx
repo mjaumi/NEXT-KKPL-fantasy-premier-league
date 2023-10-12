5f32bf928653013c59cf1576453d33a3c5d98e4c
mjaumi <mjaumi2864@gmail.com>
2023-10-12 21:15:50 +0600

    play match page created

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,6 +1,14 @@
 import SigninForm from '@/components/SigninComponents/SigninForm';
+import { Metadata } from 'next';
+
+// defining sign in page metadata here
+export const metadata: Metadata = {
+  title: 'Sign In || Khawondar Kha Premier League',
+  description: `khawondar kha premier league's sign in page`,
+};
 
 export default function SigninPage() {
+  // rendering the signin page here
   return (
     <>
       <SigninForm />
---
+++ src/app/play-match/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from 'next';
+
+// defining play match page metadata here
+export const metadata: Metadata = {
+  title: 'Play Match || Khawondar Kha Premier League',
+  description: `khawondar kha premier league's play match page`,
+};
+
+export default function PlayMatchPage() {
+  // rendering the play match page here
+  return (
+    <>
+      <p>play match</p>
+    </>
+  );
+}
