07b602d4912b5d71a4ad091d291a8b20e3698020
mjaumi <mjaumi2864@gmail.com>
2023-10-14 20:32:41 +0600

    matches page design done with responsiveness

--- src/app/matches/page.tsx
+++ src/app/matches/page.tsx
@@ -1,3 +1,6 @@
+import MatchCard from '@/components/shared/Cards/MatchCard';
+import SectionTitle from '@/components/shared/SectionTitle';
+import { match2CardData } from '@/constants/matchCardData';
 import { Metadata } from 'next';
 
 // defining play match page metadata here
@@ -9,8 +12,16 @@ export const metadata: Metadata = {
 export default function MatchesPage() {
   // rendering the play match page here
   return (
-    <>
-      <p>matches page</p>
-    </>
+    <section className='pb-20 w-[90%] lg:w-4/5 max-w-[1500px] mx-auto space-y-10'>
+      <SectionTitle title='Upcoming Matches' />
+
+      <div className='space-y-8'>
+        <MatchCard matchData={match2CardData} />
+        <MatchCard matchData={match2CardData} />
+        <MatchCard matchData={match2CardData} />
+        <MatchCard matchData={match2CardData} />
+        <MatchCard matchData={match2CardData} />
+      </div>
+    </section>
   );
 }
