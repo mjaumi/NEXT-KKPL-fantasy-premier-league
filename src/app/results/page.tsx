a775b914e57ae541253d8a1536cd812a4b8fd570
mjaumi <mjaumi2864@gmail.com>
2023-10-17 18:38:08 +0600

    results page design done with responsiveness

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,4 +1,6 @@
+import MatchCard from '@/components/shared/Cards/MatchCard';
 import SectionTitle from '@/components/shared/SectionTitle';
+import { match1CardData } from '@/constants/matchCardData';
 import { Metadata } from 'next';
 
 // defining play match page metadata here
@@ -12,6 +14,14 @@ export default function ResultsPage() {
   return (
     <section className='pb-20 w-[90%] lg:w-4/5 max-w-[1500px] mx-auto space-y-10'>
       <SectionTitle title='Match Results' />
+
+      <div className='space-y-8'>
+        <MatchCard matchData={match1CardData} />
+        <MatchCard matchData={match1CardData} />
+        <MatchCard matchData={match1CardData} />
+        <MatchCard matchData={match1CardData} />
+        <MatchCard matchData={match1CardData} />
+      </div>
     </section>
   );
 }
