a831c63bbea65ca51aac74d2b233cfeb572664c7
mjaumi <mjaumi2864@gmail.com>
2023-10-30 20:00:24 +0600

    metadata added to statistics page

--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from 'next';
+
+// defining live match page metadata here
+export const metadata: Metadata = {
+  title: 'Statistics || Khawondar Kha Premier League',
+  description: `khawondar kha premier league's statistics page`,
+};
+
 export default function StatisticsPage() {
   // rendering statistics page here
   return (
