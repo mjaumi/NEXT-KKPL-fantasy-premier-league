60ab1036e4dadf24c87402d289e70499d10a247a
mjaumi <mjaumi2864@gmail.com>
2023-11-10 02:56:33 +0600

    points table page metadata added

--- src/app/point-table/page.tsx
+++ src/app/point-table/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from 'next';
+
+// defining live match page metadata here
+export const metadata: Metadata = {
+  title: 'Points Table || Khawondar Kha Premier League',
+  description: `khawondar kha premier league's points table page`,
+};
+
 export default function PointsTablePage() {
   // rendering the points table page here
   return (
