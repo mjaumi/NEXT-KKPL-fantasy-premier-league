2c2d2636eaed9791fbcea9f4a3353beeb7171a18
mjaumi <mjaumi2864@gmail.com>
2023-11-19 18:30:01 +0600

    metadata added to create team page

--- src/app/create-team/page.tsx
+++ src/app/create-team/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from 'next';
+
+// defining live match page metadata here
+export const metadata: Metadata = {
+  title: 'Create Team || Khawondar Kha Premier League',
+  description: `khawondar kha premier league's create team page`,
+};
+
 export default function CreateTeamPage() {
   // rendering the create team page here
   return (
