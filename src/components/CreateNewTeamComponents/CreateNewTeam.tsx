1736693f2567cb7ede0e14c9375a0a212fc18719
mjaumi <mjaumi2864@gmail.com>
2023-11-29 21:29:42 +0600

    create new team component added

---
+++ src/components/CreateNewTeamComponents/CreateNewTeam.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreateNewTeam = () => {
+  // rendering create new team component here
+  return (
+    <div>
+      <p>new team</p>
+    </div>
+  );
+};
+
+export default CreateNewTeam;
