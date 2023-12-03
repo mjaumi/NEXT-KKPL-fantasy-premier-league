af9a6d45c098c65aec36fb5119fda6074ec9c227
mjaumi <mjaumi2864@gmail.com>
2023-12-03 15:58:12 +0600

    added some css classes into the create new team component

--- src/components/CreateNewTeamComponents/CreateNewTeam.tsx
+++ src/components/CreateNewTeamComponents/CreateNewTeam.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CreateNewTeam = () => {
   // rendering create new team component here
   return (
-    <section>
+    <section className='w-[90%] lg:w-4/5 max-w-[1500px] mx-auto pb-10'>
       <p>new team</p>
     </section>
   );
