4efa0c3d7756ab2ebbd106fd307a53c0beb5f267
mjaumi <mjaumi2864@gmail.com>
2023-12-04 19:04:29 +0600

    added section title into create new team page

--- src/components/CreateNewTeamComponents/CreateNewTeam.tsx
+++ src/components/CreateNewTeamComponents/CreateNewTeam.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import SectionTitle from '../shared/SectionTitle';
 
 const CreateNewTeam = () => {
   // rendering create new team component here
   return (
     <section className='w-[90%] lg:w-4/5 max-w-[1500px] mx-auto pb-10'>
+      <SectionTitle title='Create New Team' />
       <p>new team</p>
     </section>
   );
