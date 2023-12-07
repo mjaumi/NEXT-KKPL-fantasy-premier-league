a5e0a7b9385bcba2a5a942c6a14e4d584887ec62
mjaumi <mjaumi2864@gmail.com>
2023-12-07 17:00:19 +0600

    create new team form component integrated

--- src/components/CreateNewTeamComponents/CreateNewTeam.tsx
+++ src/components/CreateNewTeamComponents/CreateNewTeam.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SectionTitle from '../shared/SectionTitle';
+import CreateNewTeamForm from './CreateNewTeamForm';
 
 const CreateNewTeam = () => {
   // rendering create new team component here
   return (
     <section className='w-[90%] lg:w-4/5 max-w-[1500px] mx-auto pb-10'>
       <SectionTitle title='Create New Team' />
-      <p>new team</p>
+
+      <CreateNewTeamForm />
     </section>
   );
 };
