02df0165daab787a039cfe7419d7139a67410f16
mjaumi <mjaumi2864@gmail.com>
2023-12-08 02:03:51 +0600

    added some classnames into new team form component

--- src/components/CreateNewTeamComponents/CreateNewTeamForm.tsx
+++ src/components/CreateNewTeamComponents/CreateNewTeamForm.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const CreateNewTeamForm = () => {
   // rendering create new team form component here
-  return <div></div>;
+  return (
+    <div className='w-full md:w-4/5 mx-auto mt-8 p-5 bg-KKPL-dark-purple border border-KKPL-light-red'></div>
+  );
 };
 
 export default CreateNewTeamForm;
