b2c9bb4d5a78f456b2de1f71f157867c81c6f745
mjaumi <mjaumi2864@gmail.com>
2024-01-03 01:22:52 +0600

    section title added to match center

--- src/app/match-center/page.tsx
+++ src/app/match-center/page.tsx
@@ -1,1 +1,5 @@
-export default function MatchCenterPage() {}
+import SectionTitle from '@/components/shared/SectionTitle';
+
+export default function MatchCenterPage() {
+  return <SectionTitle title='Match Center' />;
+}
