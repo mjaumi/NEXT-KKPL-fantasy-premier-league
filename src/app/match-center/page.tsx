68d51b0aba47283c991fec6636d75542fd74978f
mjaumi <mjaumi2864@gmail.com>
2024-01-04 18:12:51 +0600

    added some comments into the match center page

--- src/app/match-center/page.tsx
+++ src/app/match-center/page.tsx
@@ -1,5 +1,6 @@
 import SectionTitle from '@/components/shared/SectionTitle';
 
 export default function MatchCenterPage() {
+  // rendering match center page here
   return <SectionTitle title='Match Center' />;
 }
