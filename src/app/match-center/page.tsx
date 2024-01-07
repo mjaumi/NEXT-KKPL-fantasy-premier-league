63e1d21961f4331abf5115419ee9cdac69065490
mjaumi <mjaumi2864@gmail.com>
2024-01-07 17:53:35 +0600

    added metadata into match center page

--- src/app/match-center/page.tsx
+++ src/app/match-center/page.tsx
@@ -1,4 +1,11 @@
 import SectionTitle from '@/components/shared/SectionTitle';
+import { Metadata } from 'next';
+
+// defining match center page metadata here
+export const metadata: Metadata = {
+  title: 'Match Center || Khawondar Kha Premier League',
+  description: `khawondar kha premier league's match center page`,
+};
 
 export default function MatchCenterPage() {
   // rendering match center page here
