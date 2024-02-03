57c8af362a5b55cfcfc98ddd15e908b30335bc4a
mjaumi <mjaumi2864@gmail.com>
2024-02-03 16:51:03 +0600

    formik form added to create new team page

--- src/components/CreateNewTeamComponents/CreateNewTeamForm.tsx
+++ src/components/CreateNewTeamComponents/CreateNewTeamForm.tsx
@@ -1,11 +1,16 @@
 'use client';
 
-import React from 'react';
+import { Formik } from 'formik';
 
 const CreateNewTeamForm = () => {
   // rendering create new team form component here
   return (
-    <div className='w-full md:w-4/5 mx-auto mt-8 p-5 bg-KKPL-dark-purple border border-KKPL-light-red'></div>
+    <div className='w-full md:w-4/5 mx-auto mt-8 p-5 bg-KKPL-dark-purple border border-KKPL-light-red'>
+      <Formik
+        initialValues={{}}
+        onSubmit={(values) => console.log(values)}
+      ></Formik>
+    </div>
   );
 };
 
