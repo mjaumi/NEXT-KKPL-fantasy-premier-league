b6fe2c0301721bd707a277ae6e8a7e4c31dcfbaa
mjaumi <mjaumi2864@gmail.com>
2024-02-07 18:25:58 +0600

    form component imported

--- src/components/CreateNewTeamComponents/CreateNewTeamForm.tsx
+++ src/components/CreateNewTeamComponents/CreateNewTeamForm.tsx
@@ -1,15 +1,14 @@
 'use client';
 
-import { Formik } from 'formik';
+import { Form, Formik } from 'formik';
 
 const CreateNewTeamForm = () => {
   // rendering create new team form component here
   return (
     <div className='w-full md:w-4/5 mx-auto mt-8 p-5 bg-KKPL-dark-purple border border-KKPL-light-red'>
-      <Formik
-        initialValues={{}}
-        onSubmit={(values) => console.log(values)}
-      ></Formik>
+      <Formik initialValues={{}} onSubmit={(values) => console.log(values)}>
+        {() => <Form></Form>}
+      </Formik>
     </div>
   );
 };
