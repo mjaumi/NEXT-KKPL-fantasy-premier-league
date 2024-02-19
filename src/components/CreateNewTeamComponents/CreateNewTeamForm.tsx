b37a5680dade87a10a44ad7772c34becf675c0e1
mjaumi <mjaumi2864@gmail.com>
2024-02-19 18:38:12 +0600

    team name input added

--- src/components/CreateNewTeamComponents/CreateNewTeamForm.tsx
+++ src/components/CreateNewTeamComponents/CreateNewTeamForm.tsx
@@ -1,13 +1,25 @@
 'use client';
 
 import { Form, Formik } from 'formik';
+import KKPLInputField from '../shared/Inputs/KKPLInputField';
 
 const CreateNewTeamForm = () => {
   // rendering create new team form component here
   return (
     <div className='w-full md:w-4/5 mx-auto mt-8 p-5 bg-KKPL-dark-purple border border-KKPL-light-red'>
       <Formik initialValues={{}} onSubmit={(values) => console.log(values)}>
-        {() => <Form className='space-y-5'></Form>}
+        {() => (
+          <Form className='space-y-5'>
+            <div>
+              <KKPLInputField
+                type='text'
+                name='country'
+                label={`Teams's Name`}
+                placeholder={`Enter Team's Name Here...`}
+              />
+            </div>
+          </Form>
+        )}
       </Formik>
     </div>
   );
