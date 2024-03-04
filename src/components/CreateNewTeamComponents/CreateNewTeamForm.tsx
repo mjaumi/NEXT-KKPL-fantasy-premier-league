d411dbb00f036fc201db4fd8bf88257d3e4db443
mjaumi <mjaumi2864@gmail.com>
2024-03-04 18:29:05 +0600

    team name initial value added

--- src/components/CreateNewTeamComponents/CreateNewTeamForm.tsx
+++ src/components/CreateNewTeamComponents/CreateNewTeamForm.tsx
@@ -7,7 +7,12 @@ const CreateNewTeamForm = () => {
   // rendering create new team form component here
   return (
     <div className='w-full md:w-4/5 mx-auto mt-8 p-5 bg-KKPL-dark-purple border border-KKPL-light-red'>
-      <Formik initialValues={{}} onSubmit={(values) => console.log(values)}>
+      <Formik
+        initialValues={{
+          team_name: '',
+        }}
+        onSubmit={(values) => console.log(values)}
+      >
         {() => (
           <Form className='space-y-5'>
             <div>
